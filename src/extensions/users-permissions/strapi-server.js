bc2ce113ba9f65afc7728a97332be0e88e2d4675
Santana20 <sstfmscontreras@gmail.com>
2023-04-27 13:19:10 -0500

    customizando endpoints de user para listar fav cloths

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -13,11 +13,48 @@ module.exports = (plugin) => {
         })
     }
 
+    plugin.controllers.user.favCloths = async (ctx) => {
+        if (!ctx.state.user || !ctx.state.user.id) {
+            return ctx.response.status = 401;
+        }
+
+        await strapi
+        .query('plugin::users-permissions.user')
+        .findOne({
+            select: [],
+            where: {id: ctx.state.user.id},
+            populate: { 
+                cloths: {
+                    select: ['*'],
+                    populate: {
+                        img: {
+                            select: ['url']
+                        },
+                        sizes: true
+                    }
+                  }
+            },
+        })
+        .then((res) => {
+            ctx.response.status = 200;
+            ctx.response.body = res
+        });
+    }
+
     plugin.routes['content-api'].routes.push(
         {
             method: "PUT",
             path: "/user/me",
             handler: "user.updateMe",
+            config: {
+                prefix: "",
+                policies: []
+            }
+        },
+        {
+            method: "GET",
+            path: "/user/me/fav_cloths",
+            handler: "user.favCloths",
             config: {
                 prefix: "",
                 policies: []
